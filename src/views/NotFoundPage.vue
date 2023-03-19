f73897b9f0026e894bec5b0bb2b881a8cb162dc2
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-03-19 23:07:12 +0100

    added the svg to the not found component

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Not found</h1>
+  <v-container class="mx-auto text-center">
+ <h1>Oops Page Not Found ....</h1>
+  <v-img
+  src="../assets/notfound.svg"
+  max-width="500px"
+  class="mx-auto"
+  >
+
+  </v-img>
+  </v-container>
+ 
 </template>
 
 <script>
