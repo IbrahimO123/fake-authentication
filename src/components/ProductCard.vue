2a9df066f96fe99b1e3d6e2d0567510f527daa0d
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-03-25 22:03:01 +0100

    added more breakpoint for better user experience

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-responsive>
     <v-row>
-      <v-col v-for="product in products" md="4" cols="12" :key="product.title">
+      <v-col v-for="product in products" sm="6" md="4" lg="3" cols="12" :key="product.title">
         <v-card>
           <v-img :src="product.images[0]" height="200px" cover></v-img>
           <template v-slot:title> {{ product.title }} </template>
@@ -11,7 +11,7 @@
           </template>
           <template v-slot:text
             ><div>
-              <div>{{ product.description.substring(0, 30) }} ...</div>
+              <div>{{ product.description.substring(0, 20) }} ...</div>
               <div class="text-body-2">
                 <span>Price: {{ product.price }}</span>
                 <span class="mx-5">Stock: {{ product.stock }}</span>
