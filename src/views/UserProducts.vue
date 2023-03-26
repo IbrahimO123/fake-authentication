2eca499c52cf9adb4851b2f15ddde51e63fffdd1
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-03-26 13:41:08 +0100

    added a user products component for selected products

---
+++ src/views/UserProducts.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <template v-if="!loading">
+      <div>
+        <span
+          >{{ this.$store.state.user.firstname || "User" }} Products
+          <p><small>Checkout Now</small></p>
+        </span>
+        <div class="under">
+          <h3>Under development</h3>
+          <h5>Amazing features coming out soon.....</h5>
+        </div>
+      </div>
+      <ProductCard :products="products" />
+    </template>
+    <template v-else>
+      <v-text-field
+        color="bg-white text-warning"
+        :loading="loading"
+        disabled
+      ></v-text-field>
+      <p class="text-white">Loading... Please wait</p>
+    </template>
+  </v-container>
+</template>
+
+<script>
+import ProductCard from "@/components/ProductCard.vue";
+export default {
+  name: "UserProducts",
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      loading: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.under {
+  margin-top: 10rem;
+  color:rgb(114, 106, 106);
+  display: grid;
+  place-items: center;
+  text-align: center;
+}
+</style>
